1cffa0d87d77fa99b9dcf96f9e73a499bec68468
Sergey Nartov <nse1988@rambler.ru>
2022-07-26 15:31:25 +0300

    prepare to launch

--- server/db/database.js
+++ server/db/database.js
@@ -7,21 +7,8 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    port: process.env.DB_PORT,
+    host: 'localhost',
     dialect: 'postgres',
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+  }
 };
